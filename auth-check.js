ba3b84b0197051f2292102be83171d6b1ce45462
Raika12345 <160618471+Raika12345@users.noreply.github.com>
2025-10-07 16:16:19 -0300

    auth-check.js

--- auth-check.js
+++ auth-check.js
@@ -7,13 +7,13 @@ onAuthStateChanged(auth, (user) => {
     
     if (!user) {
         // Se não está logado e não está nas páginas de login/cadastro
-        if (!currentPage.includes('login.html') && !currentPage.includes('cadastro.html')) {
-            window.location.href = 'login.html';
+        if (!currentPage.includes('login-:page.html') && !currentPage.includes('cadastro-page.html')) {
+            window.location.href = 'login-:page.html';
         }
     } else {
         // Se está logado e está nas páginas de login/cadastro, redireciona para index
-        if (currentPage.includes('login.html') || currentPage.includes('cadastro.html')) {
-            window.location.href = 'index.html';
+        if (currentPage.includes('login-:page.html') || currentPage.includes('cadastro-page.html')) {
+            window.location.href = 'index-page.html';
         }
     }
 });
